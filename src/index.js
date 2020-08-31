bb035d4ea4ad20f8056f0bfc5db7920df244296f
Abhishek Kumar Pandey <abhishekpandey.kr@gmail.com>
2020-08-31 08:44:29 +0530

    console multiple values handing added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const errorColor = "color:red;font-size:25px";
 const successColor = "color:green;font-size:20px;"
 const initializeColor = "color:yellow; font-size:45px"
 
-const errorLogger = (val) => {
-    console.error(`%c${val}`, errorColor)
+const errorLogger = (...val) => {
+    console.error(`%c${JSON.stringify(val)}`, errorColor)
 };
 
-const successLogger = (val) => {
-    console.log(`%c${val}`, successColor)
+const successLogger = (...val) => {
+    console.log(`%c${JSON.stringify(val)}`, successColor)
 }
 
-const warningLogger = (val) => {
-    console.warn(`%c${val}`, initializeColor)
+const warningLogger = (...val) => {
+    console.warn(`%c${JSON.stringify(val)}`, initializeColor)
 }
 module.exports.errorLogger = errorLogger;
 module.exports.successLogger = successLogger;
